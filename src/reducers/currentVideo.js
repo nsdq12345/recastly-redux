b64c009aca6817aca6e77712f401385994d7af7b
nsdq12345 <yeungjian@gmail.com>
2019-07-31 15:46:21 -0700

    fixed bug where state not defined in currentVideo if action does not have action.video

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -5,6 +5,10 @@ var currentVideoReducer = (state=null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
   if (action.type === 'CHANGE_VIDEO') {
 
+    if (!action.video) {
+      return state;
+    }
+
     return action.video;
   }
 
